49e7e4aa510e9a88fbb5dd9407a03be7f166b6f5
cwill83247 <breakthrumusic@hotmail.com>
2023-01-06 10:32:25 +0000

    Add: Adding Home templates and home app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',                            #added from all auth docs
     'allauth.account',                  #added from all auth docs
     'allauth.socialaccount',            #added from all auth docs allows user to login via social media, and we can trakc activity more effectivley
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

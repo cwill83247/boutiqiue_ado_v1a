b8c723124027b8a4fe1edc4677d147cc1a14fe89
cwill83247 <breakthrumusic@hotmail.com>
2023-01-08 13:01:11 +0000

    Add: Adding the prodcut details functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product                     # importing the Prodcuts class from models.py  
 
 # Create your views here.
@@ -14,3 +14,15 @@ def all_products(request):                                  #creating a function
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):                                  #creating a function called all_prodcuts
+    """ A view to return prodcuts, sort and search """
+
+    product = get_object_or_404(Product, pk=product_id)                  #creating a variable called product that holds the Product
+
+    context = {                                         # ??? Not sure what this does ???                            
+        'product': product,
+
+    }
+
+    return render(request, 'products/product_detail.html', context)

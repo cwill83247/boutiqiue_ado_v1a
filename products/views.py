9cdf40a0ae53f86749fd833352834dd2da4e65d3
cwill83247 <breakthrumusic@hotmail.com>
2023-01-13 09:28:22 +0000

    Add: Adding search functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages              # had to improt this as part of search 
 from .models import Product                     # importing the Prodcuts class from models.py  
+from django.db.models import Q 
 
 # Create your views here.
 
@@ -7,9 +9,22 @@ def all_products(request):                                  #creating a function
     """ A view to return prodcuts, sort and search """
 
     products = Product.objects.all ()                    #creating a variable called prpdcuts that holds all of the objects in Product
+    query = None                                    # added as part of search to set variable to none
+
+
+    if request.GET:                                       # start of search element 
+        if 'q' in request.GET:                            # we have set q as the text input in the form - hence why we use q here   
+            query = request.GET['q']                        # value of q gets set to a variable called query --- 
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)    # Q is part of DJANGO and allows an OR search so searches in description or name fields , note the i at the start
+            products = products.filter(queries)                                     #end of search element  return prpdcuts based on queries variable
 
     context = {                                         # ??? Not sure what this does ???                            
         'products': products,
+        'search_term': query,                           # added for search ? unsure why 
 
     }
 

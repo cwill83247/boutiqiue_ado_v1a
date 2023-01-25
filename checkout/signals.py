dd3c7b263b0bc7c67265b7dd38f8a1a0efe10574
cwill83247 <breakthrumusic@hotmail.com>
2023-01-25 15:42:51 +0000

    Add: Adding payment functionality

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,10 +1,10 @@
 from django.db.models.signals import post_save, post_delete    # send a message/signal after save or delete in this checkout app 
 from django.dispatch import receiver   #this receives the messages 
 
-from .models import OrderLineItem
+from .models import OrderLineItem    # specifying what/where we ar elistening for the save or delet 
 
-@receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, created, **kwargs):
+@receiver(post_save, sender=OrderLineItem)             # we are receiving signals form save on the orderlineitems   do the below 
+def update_on_save(sender, instance, created, **kwargs):     #kwargs -- keyword arguments 
     """
     Update order total on lineitem update/create
     """
---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,75 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});            //this means creating a card part for the credit/debit card
+card.mount('#card-element');                                // this gets mounted to the section in checkout.html the div called - card-element
+
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {       //call to stripe for validation 
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');   // post errors to our div "card-errors"
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {   // is succeeds submit the form 
+                form.submit();
+            }
+        }
+    });
+});

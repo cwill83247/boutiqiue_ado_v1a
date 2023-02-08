5285000e526792811e2590dab658a2a6b076313c
cwill83247 <breakthrumusic@hotmail.com>
2023-02-08 11:48:43 +0000

    Admin of prpdcuts

---
+++ products/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):  # majority of this is standard django syntax, but references our fields from our Models.py
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):                #overridign the init method 
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]   #list comprehension syntax ..
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'

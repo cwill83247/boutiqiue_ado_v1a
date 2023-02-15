db93ac6f784b8c3924eba2b529ebac971991f2ce
cwill83247 <breakthrumusic@hotmail.com>
2023-02-15 12:00:09 +0000

    Add: Adding environment variable

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wh#_xs#d=1js&anhafkpuf)fzpvt0#q1^l=s=@ztx%%6^_y=4('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cw-boutique-ado.herokuapp.com','localhost']
 
